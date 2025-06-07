7b20dedb87308aa331b4d48d9af9841d9e544249
Felix Ding <felixding1989@gmail.com>
2025-06-07 16:20:11 -0700

    adds auto codegen for shader glsl files

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Build = std.Build;
+const Entry = std.fs.Dir.Walker.Entry;
 
 const Bin = struct {
     name: []const u8,
@@ -21,6 +22,7 @@ pub fn build(b: *std.Build) void {
 
     const src_dir = std.fs.cwd().openDir("src", .{ .iterate = true }) catch unreachable;
     var walker = src_dir.walk(b.allocator) catch unreachable;
+    var shader_dir = src_dir.openDir("shaders", .{}) catch unreachable;
     defer walker.deinit();
     while (walker.next() catch unreachable) |file| {
         if (std.mem.eql(u8, ".zig", std.fs.path.extension(file.path))) {
@@ -29,10 +31,18 @@ pub fn build(b: *std.Build) void {
             if (std.mem.startsWith(u8, file.path, "shaders/") or std.mem.eql(u8, name, "math")) {
                 continue;
             }
-            build_bin(b, .{
-                .name = name,
-                .path = path,
-            }, sokol_dep, target, optimize, no_bin);
+            build_bin(
+                b,
+                .{
+                    .name = name,
+                    .path = path,
+                },
+                sokol_dep,
+                target,
+                optimize,
+                &shader_dir,
+                no_bin,
+            );
         }
     }
 }
@@ -44,6 +54,7 @@ pub fn build_bin(
     sd: *Build.Dependency,
     target: Build.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
+    shader_dir: *std.fs.Dir,
     no_bin: bool,
 ) void {
     const bin_to_add = b.addExecutable(.{
@@ -52,6 +63,29 @@ pub fn build_bin(
         .optimize = optimize,
         .root_source_file = bin.path,
     });
+
+    // We should build the generated shader file either way
+    // But we would only do it if the zig file is out of date
+    const bin_name = bin.name;
+    if (shader_dir.statFile(b.fmt("{s}.glsl", .{bin_name}))) |gl_stat| {
+        const zig_file_out_of_date = date: {
+            if (shader_dir.statFile(b.fmt("{s}.glsl.zig", .{bin_name}))) |zig_stat| {
+                const gl_last_mod = gl_stat.mtime;
+                const zig_last_mod = zig_stat.mtime;
+                break :date gl_last_mod > zig_last_mod;
+            } else |_| {
+                break :date true;
+            }
+        };
+
+        if (zig_file_out_of_date) {
+            const run_command = run_sokol_shdc(b, bin_name);
+            bin_to_add.step.dependOn(&run_command.step);
+        }
+    } else |_| {
+        // ignore, assume file does not exist
+    }
+
     bin_to_add.root_module.addImport("sokol", sd.module("sokol"));
     const selective_install = b.addInstallArtifact(bin_to_add, .{});
     const run = b.addRunArtifact(bin_to_add);
@@ -78,4 +112,23 @@ pub fn build_bin(
     } else {
         build_step.dependOn(&selective_install.step);
     }
+}
+
+pub fn run_sokol_shdc(
+    b: *Build,
+    name: []const u8,
+) *Build.Step.Run {
+    const sokol_shdc = b.addSystemCommand(&.{
+        "sokol-shdc",
+        "-i",
+        b.fmt("src/shaders/{s}.glsl", .{name}),
+        "-o",
+        b.fmt("src/shaders/{s}.glsl.zig", .{name}),
+        "-l",
+        "glsl410:glsl300es:hlsl5:metal_macos:wgsl",
+        "-f",
+        "sokol_zig",
+    });
+
+    return sokol_shdc;
 }
